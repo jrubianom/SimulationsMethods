95ef4a87fe435f4c17f22420a889a7904ccc7e6a
Alejandro Salas <70211428+UNasalas@users.noreply.github.com>
2022-11-23 22:52:30 -0500

    Update plot_Flift_U.py

--- Taller2/Punto4/plot_Flift_U.py
+++ Taller2/Punto4/plot_Flift_U.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-U, U2, Fl, Fd = np.loadtxt("Flift_Fdrag_vs_Ufan.dat", unpack=True)
+U, Rn, Fl = np.loadtxt("Flift_Fdrag_vs_Ufan.dat", unpack=True)
 
 plt.plot(U, Fl)
 plt.xlabel("Velocidad [celdas/click]")
